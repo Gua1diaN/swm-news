5cf196755169d14632651fcab0bec095ba476609
JialinYangSWM <137037123+JialinYangSWM@users.noreply.github.com>
2023-07-18 16:02:54 +0800

    change test

--- src/profile/__tests__/Profile.test.tsx
+++ src/profile/__tests__/Profile.test.tsx
@@ -5,5 +5,5 @@ it('Should render Profile component', async () => {
     // render(<Profile />);
     // const articleElement = await screen.findByTestId('whole-article');
     // expect(articleElement).toBeInTheDocument();
-    // test11122223333
+    // test1112222333344444
 });
